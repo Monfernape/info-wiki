e5d9e293e5cf674ebbba816fe85c9b4161dd8013
Monfernape <engr.usmankhalil@gmail.com>
2023-12-05 21:02:05 +0500

    fix: styled contacts page

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -4,17 +4,25 @@ import { CardModule } from "primeng/card";
 @Component({
   selector: "app-contacts",
   standalone: true,
-  styleUrl: "./contacts.component.css",
   imports: [CardModule],
   template: `
-    <p-card
-      header="Company: Super Company"
-      [style]="{ width: '25rem', margin: '1em', 'margin-bottom': '2em' }"
-    >
-      <p>
-        Address: Hawaii, USA
-      </p>
-    </p-card>
+    <style>
+      .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin-top: 1em;
+      }
+    </style>
+    <div class="container">
+      <p-card
+        header="Company: Super Company"
+        [style]="{ width: '25rem', margin: '1em', 'margin-bottom': '2em' }"
+      >
+        <p>Address: Hawaii, USA</p>
+      </p-card>
+    </div>
   `,
 })
 export class ContactsPage {}
