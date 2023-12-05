5b14fd35a2c31ada0b3e4ad766326e67682a7939
Monfernape <engr.usmankhalil@gmail.com>
2023-12-05 20:57:40 +0500

    fix: styled footer

--- src/app/layout/footer.component.ts
+++ src/app/layout/footer.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MenubarModule } from "primeng/menubar";
 import { DockModule } from "primeng/dock";
 
 @Component({
@@ -8,6 +7,19 @@ import { DockModule } from "primeng/dock";
   standalone: true,
   imports: [CommonModule, DockModule],
   template: `
+  <style>
+    footer {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      height: 2.5rem;
+      background-color: #f0f0f0;
+      border-top: 1px solid #e5e5e5;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
     <footer>
       &#169; SuperCompany 2023
     </footer>
